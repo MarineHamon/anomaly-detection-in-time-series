6e7a35c35428c458cede718e7b5eecd97f732a86
Marine Hamon <marinehamon30@gmail.com>
2024-11-25 21:52:26 +0100

    english version

--- utils_fonctions.py
+++ utils_fonctions.py
@@ -5,9 +5,9 @@ import json
 
 def read_dataset(file):
     """
-    Lecture de la série temporelle et des labels associés dans un pandas Dataframe.
+    Reading the time series and associated labels in a pandas Dataframe.
 
-    file : nom du chemin permettant d'accéder à la série étudiée.
+    file : name of the path used to access the series studied.
     """
     df = pd.read_csv(file, header=0) 
     return df
@@ -15,20 +15,20 @@ def read_dataset(file):
 
 def sliding_window(TS, size):
     """
-    Fenêtrage de la série temporelle.
+    Time series windowing.
 
-    TS : une série temporelle sous forme de pandas.Series,
-    size : longueur de la fenêtre.
+    TS : a time series in the form of pandas.Series.
+    size : window length.
     """
 
-    # Séquence de valeurs allant de 0 jusqu'au nombre de sous-séquences créées moins une, soit len(TS) - size.
+    # Sequence of values from 0 to the number of subsequences created minus one, i.e. len(TS) - size.
     rows = range(len(TS) - size + 1)
-    # Création d'un pandas Dataframe vide, qui contiendra les sous-séquences de la série temporelle. 
-    # Les lignes sont numérotées à l'aide de la séquence 'rows' et 
-    # les colonnes sont nommées 'value0' à 'value{size-1}' où size représente la taille du fenêtrage.
+    # Creation of an empty pandas Dataframe, which will contain the subsequences of the time series. 
+    # Lines are numbered using the sequence ‘rows’ and 
+    # the columns are named ‘value0’ to ‘value{size-1}’ where size represents the window size.
     df_windows = pd.DataFrame(index = [f"w{i:06d}" for i in rows], 
                               columns = [f"value{i}" for i in range(size)]) 
-    # Remplissage du tableau avec les sous-séquences extraites de la série temporelle.
+    # Fill in the table with the subsequences extracted from the time series.
     for i in rows :
         df_windows.iloc[i,:] = TS[(i) : (i + size)]
 
@@ -37,28 +37,28 @@ def sliding_window(TS, size):
 
 def transformation_label(labels, size):
     """
-    Fenêtrage des labels de la série temporelle étudiée.
+    Windowing of labels of the time series studied.
 
-    labels : labels de la série temporelle, sous forme de pandas.Series,
-    size : longueur de la fenêtre.
+    labels : time series labels, in the form of pandas.Series.
+    size : window length.
     """
 
-    # Définition du nombre de sous-séquences créées par fenêtrage.
+    # Definition of the number of subsequences created by windowing.
     new_size_labels = len(labels) - size + 1
-    # Initialisation à 0 de la liste correspondant aux labels agrégés, de longueur 'new_size_labels'.
+    # Initialise the list of aggregated labels to 0, of length ‘new_size_labels’.
     new_labels = [0]*new_size_labels 
 
-    # Boucle permettant d'agréger une sous-séquence de labels en un seul label.
+    # Loop used to aggregate a subsequence of labels into a single label.
     for i in range(new_size_labels) :
-        # Somme des valeurs d'une sous-séquence de labels.
+        # Sum of the values of a label subsequence.
         sum_window = labels[(i) : (i + size)].sum()
-        # Si la somme est nulle, nous conservons cette valeur. Cela signifie qu'il n'y a pas d'anomalies dans la sous-séquence concernée.
-        # Sinon, elle est remise à 1, signifiant qu'au moins un point de données de la sous-séquence concernée est une anomalie.
+        # If the sum is zero, we retain this value. This means that there are no anomalies in the subsequence concerned.
+        # Otherwise, it is reset to 1, meaning that at least one data point in the sub-sequence concerned is an anomaly.
         if sum_window > 0:
             new_labels[i] = 1
-    # Transformation de la liste en pandas Dataframe.
+    # Transformation of the list into pandas Dataframe.
     new_labels_df = pd.DataFrame(new_labels)
-    # Renommage de la colonne contenant l'agrégation des labels.
+    # Renames the column containing the label aggregation.
     new_labels_df.columns = ["label"]
     return new_labels_df
 
@@ -67,49 +67,49 @@ def transformation_label(labels, size):
 
 def pivot4tsfresh(data):
     """
-    Pivotage de la table de données comportant les sous-séquences de longueur 'size' afin que la fonction 'extract_features' soit applicable.
+    Pivot the data table containing subsequences of length 'size' so that the 'extract_features' function can be applied.
 
-    data : tableau contenant, en lignes, les sous-séquences issues du fenêtrage de la série temporelle.
+    data : table containing, in rows, the subsequences resulting from the windowing of the time series.
     """
 
-    # Ajout de la colonne 'TSname', correspondant aux noms des sous-séquences extraites, au tableau obtenu par fenêtrage.
+    # Addition of the ‘TSname’ column, corresponding to the names of the extracted subsequences, to the table obtained by windowing.
     new_data = data.reset_index().rename(columns={'index':'TSname'})
-    # Pivotage du tableau de telle sorte que la nouvelle table contienne deux variables comme index : 
-    #   - 'TSname' : noms des sous-séquences, 
-    #   - 'time' : timestamps des sous-séquences, allant de 0 à size-1, 
-    # et une colonne 'value' contenant la valeur de la sous-séquence au nom 'TSname' et au temps 'time'.
+    # Rotate the table so that the new table contains two variables as indexes: 
+    #   - 'TSname': names of the subsequences, 
+    #   - 'time': subsequence timestamps, ranging from 0 to size-1, 
+    # and a 'value' column containing the value of the subsequence with name 'TSname' and time 'time'.
     new_data = pd.wide_to_long(new_data, stubnames='value', i='TSname', j='time').sort_values(['TSname', 'time'])
-    # Redéfinition des index du tableau (et donc ajout des variables 'TSname' et 'time' comme colonnes).
+    # Redefinition of the array indexes (and therefore addition of the variables ‘TSname’ and ‘time’ as columns).
     new_data.reset_index(inplace=True)
-    # Les valeurs des données sont converties en float.
+    # Data values are converted to float.
     new_data['value'] = new_data['value'].astype(float)
     return new_data
 
 
 
 
-def add_json_file(file_name, nv_dict, ts_name):
+def add_json_file(file_name, new_dict, ts_name):
     """
-    Fonction permettant de joindre les résultats provenant de plusieurs séries temporelles dans un fochier .json.
+    Function for combining results from several time series in a .json file.
 
-    file_name : nom du fichier .json dans lequel les résultats sont conservés,
-    nv_dict : dictionnaire dont les clés sont 'TS' et 'FE' correspondant à l'approche employée et les valeurs sont des 
-    listes de liste de la forme [[auc_size1_method1, auc_size1_method2], [auc_size2_method1, auc_size2_method2], ...],
-    ts_name : nom de la série temporelle.
+    file_name : name of the .json file in which the results are stored.
+    new_dict : dictionary whose keys are 'TS' and 'FE' corresponding to the approach used and whose values are list of lists 
+               of the form [[auc_size1_method1, auc_size1_method2], [auc_size2_method1, auc_size2_method2], ...].
+    ts_name : name of the time series.
     """
-    # Vérification de l'existence du fichier.
+    # Check that the file exists.
     if os.path.exists(file_name):
-        # Si le fichier existe, les données existantes sont récupérées.
+        # If the file exists, the existing data is retrieved.
         with open(file_name, 'r', encoding='utf-8') as f1:
             dict_json = json.load(f1)
     else:
-        # Si le fichier n'existe pas, une nouvelle liste est initialisée.
+        # If the file does not exist, a new list is initialized.
         dict_json = {}
 
-    # Ajout du nouveau dictionnaire à la liste.
-    dict_json[ts_name] = nv_dict
+    # Add the new dictionary to the list.
+    dict_json[ts_name] = new_dict
     
-    # Mise à jour du fichier avec les nouvelles données.
+    # Updating the file with the new data.
     with open(file_name, 'w', encoding='utf-8') as f2:
         json.dump(dict_json, f2, ensure_ascii=False, indent=4)
 
